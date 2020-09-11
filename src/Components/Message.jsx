125553e40822ee8ff997951f34c54bd0640a1d99
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-09-11 17:55:01 +0530

    adding forwardRef

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React,{forwardRef} from 'react';
 import {CardContent,Typography,Card} from '@material-ui/core'
 import './Message.css';
 
-function Message({message,username}){
+const Message= forwardRef(({message,username},ref)=>{
 
     const isUser = username===message.username;
 
     return(
-        <div className={`message ${isUser && 'message__user'}`}>
+        <div ref={ref} className={`message ${isUser && 'message__user'}`}>
    <Card className={isUser? "message__userCard":"message__guestCard"}>
      <CardContent>
         <Typography 
@@ -23,6 +23,6 @@ function Message({message,username}){
 
 
     )
-}
+})
 
 export default Message;
