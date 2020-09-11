b138f2e12fe831f77adb5ea55678973a7d032a31
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-09-11 14:26:13 +0530

    added the state

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input,setInput]=useState("")
+  const [messages,setMessages]=useState(["hai","hello"])
+  console.log(messages);
 
   const sendMessage=()=>{
-    
+    setMessages([...messages,input]);
+    setInput("")
   }
 
-  const [input,setInput]=useState("")
-     console.log(input);
+ 
   return (
     <div className="App">
       <h1>Non Stop!🚀</h1>
       <input value={input} onChange={(event)=>setInput(event.target.value)}/>
-      <button>Send message</button>
+      <button disabled={!input} onClick={sendMessage}> Send message</button>
+          <div>{messages.map((message)=>{
+            return(
+            <li>{message}</li>
+            )
+      })}</div>
     </div>
   );
 }
