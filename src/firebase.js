ec55d852d63b59821d11cc65f3be2d4bb398a0ed
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-09-11 17:18:40 +0530

    added firebaseConfig

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAQ8TgrcydHOiO-DBkicLKZ1csqdbQew7c",
+    authDomain: "non-stop-kevinjacob2001.firebaseapp.com",
+    databaseURL: "https://non-stop-kevinjacob2001.firebaseio.com",
+    projectId: "non-stop-kevinjacob2001",
+    storageBucket: "non-stop-kevinjacob2001.appspot.com",
+    messagingSenderId: "760671925254",
+    appId: "1:760671925254:web:d51c00122fcf9dfda37f87",
+    measurementId: "G-EHHCDNTQXT"
+  };
