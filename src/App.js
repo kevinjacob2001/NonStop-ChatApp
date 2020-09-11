7feaa9f7f9929e33c069c933ff9ed27f1eecc39b
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-09-11 14:00:06 +0530

    basic setup completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-Hai there!!
+      hey friends!!! Wassup?
     </div>
   );
 }
