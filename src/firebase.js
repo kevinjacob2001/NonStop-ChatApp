15063cdbb54f8b61300bf9c4ed60bdddaf6b8387
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-09-11 17:20:41 +0530

    firebase.js ready  to export

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
-const firebaseConfig = {
+import firebase from 'firebase';
+
+const firebaseApp = firebase.initializeApp({
     apiKey: "AIzaSyAQ8TgrcydHOiO-DBkicLKZ1csqdbQew7c",
     authDomain: "non-stop-kevinjacob2001.firebaseapp.com",
     databaseURL: "https://non-stop-kevinjacob2001.firebaseio.com",
@@ -7,4 +9,7 @@ const firebaseConfig = {
     messagingSenderId: "760671925254",
     appId: "1:760671925254:web:d51c00122fcf9dfda37f87",
     measurementId: "G-EHHCDNTQXT"
-  };
+  });
+
+  const db=firebaseApp.firestore();
+  export {db};
